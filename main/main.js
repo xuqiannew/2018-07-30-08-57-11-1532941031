8e7cff3d232fe8067c447c759b6db492a619cfa6
xuqiannew <949768312@qq.com>
2018-08-03 09:42:57 +0800

    deng

--- main/main.js
+++ main/main.js
@@ -1,4 +1,31 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+  //  console.log("Debug Info");
+  //  return 'Hello World!';
+  let oneRow = ["._." , "..." , "._." , "._." ,"..." , "._." ,"._." ,"._." , "._." ,"._."];
+  let secondRow = ["|.|" , "..|" , "._|" ,"._|" ,"|_|" ,"|_." , "|_." ,"..|" ,"|_|" ,"|_|"];
+  let thirdRow = ["|_|" , "..|" ,"|_." , "._|" ,"..|" ,"._|" ,"|_|" , "..|" ,"|_|" ,"..|"];
+  let arr = str.split("");
+  let sum ="";
+  let sum1 ="";
+  let sum2 ="";
+  let num = 0;
+while(++num <= 3){
+    if(num == 1){
+        for(let i = 0 ; i < arr.length ; ++i){
+            let  index =  arr[i];
+            sum = sum + oneRow[index] + " "; 
+        }
+    }else if(num == 2){
+        for(let i = 0 ; i < arr.length ; ++i){
+            let  index =  arr[i];
+            sum1 =sum1 + secondRow[index] + " ";  
+        }
+    }else if(num == 3){
+        for(let i = 0 ; i < arr.length ; ++i){
+            let  index =  arr[i];
+            sum2 = sum2 + thirdRow[index] + " ";
+        }
+    } 
+}
+    return sum +"\n" + sum1 + "\n" + sum2 + "\n";
 };
